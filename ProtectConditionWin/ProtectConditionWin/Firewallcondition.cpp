480f8f3b4505649f008573a2a82053d9347b07bd
andreyz <andrey.zbaraschuk@nurd.com>
2018-09-20 18:13:10 +0300

    14 commit

---
+++ ProtectConditionWin/ProtectConditionWin/Firewallcondition.cpp
@@ -0,0 +1,168 @@
+#define _WIN32_WINNT 0x0500
+
+#include <windows.h>
+#include <crtdbg.h>
+#include <netfw.h>
+#include <objbase.h>
+#include <oleauto.h>
+#include <stdio.h>
+
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "oleaut32.lib")
+
+
+HRESULT WindowsFirewallInitialize(OUT INetFwProfile** fwProfile)
+{
+	HRESULT hr = S_OK;
+	INetFwMgr* fwMgr = NULL;
+	INetFwPolicy* fwPolicy = NULL;
+
+	_ASSERT(fwProfile != NULL);
+
+	*fwProfile = NULL;
+
+	// Create an instance of the firewall settings manager.
+	hr = CoCreateInstance(
+		__uuidof(NetFwMgr),
+		NULL,
+		CLSCTX_INPROC_SERVER,
+		__uuidof(INetFwMgr),
+		(void**)&fwMgr
+	);
+	if (FAILED(hr))
+	{
+		printf("CoCreateInstance failed: 0x%08lx\n", hr);
+		goto error;
+	}
+
+	// Retrieve the local firewall policy.
+	hr = fwMgr->get_LocalPolicy(&fwPolicy);
+	if (FAILED(hr))
+	{
+		printf("get_LocalPolicy failed: 0x%08lx\n", hr);
+		goto error;
+	}
+
+	// Retrieve the firewall profile currently in effect.
+	hr = fwPolicy->get_CurrentProfile(fwProfile);
+	if (FAILED(hr))
+	{
+		printf("get_CurrentProfile failed: 0x%08lx\n", hr);
+		goto error;
+	}
+
+error:
+
+	// Release the local firewall policy.
+	if (fwPolicy != NULL)
+	{
+		fwPolicy->Release();
+	}
+
+	// Release the firewall settings manager.
+	if (fwMgr != NULL)
+	{
+		fwMgr->Release();
+	}
+
+	return hr;
+}
+
+
+void WindowsFirewallCleanup(IN INetFwProfile* fwProfile)
+{
+	// Release the firewall profile.
+	if (fwProfile != NULL)
+	{
+		fwProfile->Release();
+	}
+}
+
+HRESULT WindowsFirewallIsOn(IN INetFwProfile* fwProfile, OUT BOOL* fwOn)
+{
+	HRESULT hr = S_OK;
+	VARIANT_BOOL fwEnabled;
+
+	_ASSERT(fwProfile != NULL);
+	_ASSERT(fwOn != NULL);
+
+	*fwOn = FALSE;
+
+	// Get the current state of the firewall.
+	hr = fwProfile->get_FirewallEnabled(&fwEnabled);
+	if (FAILED(hr))
+	{
+		printf("get_FirewallEnabled failed: 0x%08lx\n", hr);
+		goto error;
+	}
+
+	// Check to see if the firewall is on.
+	if (fwEnabled != VARIANT_FALSE)
+	{
+		*fwOn = TRUE;
+		printf("The firewall is on.\n");
+	}
+	else
+	{
+		printf("The firewall is off.\n");
+	}
+
+error:
+
+	return hr;
+}
+
+
+int __cdecl wmain(int argc, wchar_t* argv[])
+{
+	HRESULT hr = S_OK;
+	HRESULT comInit = E_FAIL;
+	INetFwProfile* fwProfile = NULL;
+
+	// Initialize COM.
+	comInit = CoInitializeEx(
+		0,
+		COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE
+	);
+
+	
+	if (comInit != RPC_E_CHANGED_MODE)
+	{
+		hr = comInit;
+		if (FAILED(hr))
+		{
+			printf("CoInitializeEx failed: 0x%08lx\n", hr);
+			goto error;
+		}
+	}
+
+	// Retrieve the firewall profile currently in effect.
+	hr = WindowsFirewallInitialize(&fwProfile);
+	if (FAILED(hr))
+	{
+		printf("WindowsFirewallInitialize failed: 0x%08lx\n", hr);
+		goto error;
+	}
+
+	// Is is turned on?
+	BOOL bIsOn = false;
+	hr = WindowsFirewallIsOn(fwProfile, &bIsOn);
+	if (FAILED(hr))
+	{
+		printf("WindowsFirewallIsOn failed: 0x%08lx\n", hr);
+		goto error;
+	}
+
+error:
+
+	// Release the firewall profile.
+	WindowsFirewallCleanup(fwProfile);
+
+	// Uninitialize COM.
+	if (SUCCEEDED(comInit))
+	{
+		CoUninitialize();
+	}
+
+	return 0;
+}
